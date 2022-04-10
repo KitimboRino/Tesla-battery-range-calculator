c5704568ba8bf8041da1b87901ed444c430f6b24
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-04-10 13:30:04 +0300

    Added Tesla battery container

---
+++ src/containers/TeslaBattery.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TeslaBattery.css';
+
+class TeslaBattery extends React.Component {
+    render() {
+        return (
+            <form action="">
+                <h1>Range Per Charge</h1>
+            </form>
+        )
+    }
+}
+export default TeslaBattery;
